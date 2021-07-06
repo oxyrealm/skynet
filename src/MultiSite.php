4167f797f4c09933b15fbe23cd52ae67ceb13b96
Joshua Siagian <suabahasa@gmail.com>
2021-07-06 08:35:56 +0700

    get endpoint routes collection

--- src/MultiSite.php
+++ src/MultiSite.php
@@ -2,6 +2,7 @@
 
 namespace Oxyrealm\Skynet;
 
+use Illuminate\Support\Str;
 use Oxyrealm\Asura\Models\Blog;
 
 class MultiSite {
@@ -33,5 +34,33 @@ class MultiSite {
 
 	public static function setAdminRouteUuid() {
 		update_option( 'asura_admin_route_uuid', wp_generate_uuid4() );
+	}
+
+	public static function getEndpointRoutes() {
+		$routes = app()->router->getRoutes();
+        $url = MultiSite::isMultiSite() ? ( 
+            is_ssl() ? 'https://' : 'http://'
+        ) . MultiSite::blog()->domain : rtrim( site_url(), '/' ) ;
+
+        $api_routes = [
+            'url' => $url,
+            'port' => parse_url($url)['port'] ?? null,
+            'defaults' => method_exists(app('url'), 'getDefaultParameters')
+                ? app('url')->getDefaultParameters()
+                : [],
+			'admin_route_uuid' => MultiSite::getAdminRouteUuid(),
+			'endpoint' => config('rest.endpoint').'/'.config('rest.version')
+        ];
+
+        foreach ($routes as $route) {
+            if (!empty($route['action']['as'])) {
+                $api_routes['routes'][$route['action']['as']] = [
+                    'uri' => Str::length($route['uri']) < 2 ? $route['uri'] : (string) Str::of($route['uri'])->replaceFirst( $api_routes['endpoint'] , '')->ltrim('/'),
+                    'methods' => $route['method'] === 'GET' ? ['HEAD','GET'] : [$route['method']],
+                ];
+            }
+        }
+
+        return $api_routes;
 	}
 }
